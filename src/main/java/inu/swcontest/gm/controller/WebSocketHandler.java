eeae55701c39b909f25d21fb2d8437caf93b8be5
Chani17 <cucute8878@naver.com>
2023-11-05 01:10:23 +0900

    Feat: Implement WebSocket Config & Handler

---
+++ src/main/java/inu/swcontest/gm/controller/WebSocketHandler.java
@@ -0,0 +1,13 @@
+package inu.swcontest.gm.controller;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+    }
+}
