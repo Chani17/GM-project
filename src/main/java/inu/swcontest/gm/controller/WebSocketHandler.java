9722c6e37c89cd18e94fef4dd9954b8f25073f8a
Chani17 <cucute8878@naver.com>
2023-11-23 14:41:57 +0900

    remove: remove comment in websocket handler

--- src/main/java/inu/swcontest/gm/controller/WebSocketHandler.java
+++ src/main/java/inu/swcontest/gm/controller/WebSocketHandler.java
@@ -53,7 +53,6 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     public static String getLastMessage() {
         System.out.println("CLIENTS = " + CLIENTS);
-        System.out.println("lastMessage = " + lastMessage);
 
         // total epoch:recent epoch:time
         String[] data = lastMessage.split(":");
@@ -65,6 +64,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
         int expectTime = elapsedTime * totalEpoch;
 
         String response = totalEpoch + " " + recentEpoch + " " + avgEpoch + " " + elapsedTime + " " + expectTime;
+        System.out.println("response = " + response);
         return response;
     }
 
